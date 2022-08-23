51302a9d7eca674aa5508390d3ba89016e5c0505
Antoine Puech <antoine.puech@edu.devinci.fr>
2022-08-23 15:29:29 +0200

    make first exercise

--- 01-Speed-in-mph/lib/speed_in_mph.rb
+++ 01-Speed-in-mph/lib/speed_in_mph.rb
@@ -1,2 +1,9 @@
 # TO DO: write a method that takes a float as an argument and
 # returns it converted from kph (km/h) to mph (mi/h)
+
+def speed_in_mph(kmh)
+  if kmh.class != Float && kmh.class != Integer
+    return error("Not an integer or a float")
+  end
+  return mph = kmh / 1.609
+end
--- 01-Speed-in-mph/spec/speed_in_mph_spec.rb
+++ 01-Speed-in-mph/spec/speed_in_mph_spec.rb
@@ -1,2 +1,18 @@
 # TO DO: Write the specs that ensures our speed_in_mph method
 # successfully converts speed from km/h to mi/h.
+require './lib/speed_in_mph'
+
+describe "#speed_in_mph" do
+    context 'kmh is an integer' do
+        it 'should return kmh to mph when an integer is provided' do
+            expect(20).to be_kind_of(Integer || Float)
+            expect(speed_in_mph(20)).to eq(20 / 1.609)
+            expect(speed_in_mph(20)).to be_kind_of(Float)
+        end
+    end
+    context 'kmh is not an integer' do
+        it 'should return an error' do
+            expect(speed_in_mph('hello world')).to eq('Not an integer')
+        end
+    end
+end
